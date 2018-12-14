d45b2c1811b9d2c6614f5d4d081817ff5b3b1571
David Hong <dahong@umich.edu>
2018-12-14 17:11:38 -0500

    The real initial commit.
    Copied in the code from before.

---
+++ src/CacheVariables.jl
@@ -0,0 +1,40 @@
+module CacheVariables
+
+using BSON
+
+export cache
+
+function _cachevars(ex::Expr)
+    (ex.head === :(=))   && return Symbol[ex.args[1]]
+    (ex.head === :block) && return collect(
+        Iterators.flatten([_cachevars(exi) for exi in ex.args if isa(exi,Expr)])
+    )
+    return Vector{Symbol}(undef,0)
+end
+
+macro cache(path,ex::Expr,overwrite=false)
+    vars = _cachevars(ex)
+    vardesc = join(string.(vars),", "," and ")
+    if !isfile(path) || overwrite
+        varkws  = [:($(var) = $(var)) for var in vars]
+        varlist = :($(varkws...),)
+        return quote
+            val = $(esc(ex))
+            print("Saving variables ",$vardesc," to cache file `",$path,"`...")
+            bson($path; $(esc(varlist))...)
+            println("done!")
+            val
+        end
+    else
+        vartuple = :($(vars...),)
+        return quote
+            print("Loading variables ",$vardesc," from cache file `",$path,"`...")
+            data = BSON.load($path)
+            $(esc(vartuple)) = getindex.(Ref(data),$vars)
+            println("done!")
+            $(esc(vars[end]))
+        end
+    end
+end
+
+end # module
