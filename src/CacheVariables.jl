c87eac78cae747a4cf1e93c83099f00261ad0aaa
Jonas Kersulis <kersulis@umich.edu>
2018-12-15 03:32:38 -0500

    Address dahong67/CacheVariables.jl#1

--- src/CacheVariables.jl
+++ src/CacheVariables.jl
@@ -1,6 +1,7 @@
 module CacheVariables
 
 using BSON
+import Logging: @info
 
 export @cache
 
@@ -12,26 +13,22 @@ function _cachevars(ex::Expr)
     return Vector{Symbol}(undef,0)
 end
 
-macro cache(path,ex::Expr,overwrite=false)
+macro cache(path, ex::Expr, overwrite::Bool=false)
     vars = _cachevars(ex)
-    vardesc = join(string.(vars),", "," and ")
-    if !isfile(path) || overwrite
-        varkws  = [:($(var) = $(var)) for var in vars]
-        varlist = :($(varkws...),)
-        return quote
-            val = $(esc(ex))
-            print("Saving variables ",$vardesc," to cache file `",$path,"`...")
-            bson($path; $(esc(varlist))...)
-            println("done!")
-            val
-        end
-    else
-        vartuple = :($(vars...),)
-        return quote
-            print("Loading variables ",$vardesc," from cache file `",$path,"`...")
-            data = BSON.load($path)
-            $(esc(vartuple)) = getindex.(Ref(data),$vars)
-            println("done!")
+    vardesc = join(string.(vars), "\n")
+    varkws  = [:($(var) = $(var)) for var in vars]
+    varlist = :($(varkws...),)
+    vartuple = :($(vars...),)
+
+    return quote
+        if !isfile($(esc(path))) || $(esc(overwrite))
+            @info(string("Saving to ", $(esc(path)), "\n", $(vardesc)))
+            bson($(esc(path)); $(esc(varlist))...)
+            $(esc(ex))
+        else
+            @info(string("Loading from ", $(esc(path)), "\n", $(vardesc)))
+            data = BSON.load($(esc(path)))
+            $(esc(vartuple)) = getindex.(Ref(data), $vars)
             $(esc(vars[end]))
         end
     end
